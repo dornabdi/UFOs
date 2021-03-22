03b291c204a0963c6b68426d11b7bbdc20dad4fb
Dorna Abdi <dornaabdi@Dornas-MacBook-Pro.local>
2021-03-22 12:41:03 -0500

    11.5.4 Commit

--- js/app.js
+++ js/app.js
@@ -24,3 +24,25 @@ function buildTable(data) {
 
 }
 
+function handleClick() {
+    //Get datetime value from filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Find new date if there is one, filter data using that date
+    if (date) {
+        //Apply filter to table data, keep rows that match the correct datetime
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}  
+
+// Attach event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build table when the page loads
+buildTable(tableData);
